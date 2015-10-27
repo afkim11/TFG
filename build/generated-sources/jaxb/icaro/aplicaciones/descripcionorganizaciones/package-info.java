1282790b6c90eff2e7857700223f6b8c8c7dbd4b
afkim11 <luigiter@hotmail.com>
2015-10-27 20:35:10 +0100

    Revert "p"
    This reverts commit 6ad96850b44627be7a84540b770bb8c59152c436.

--- build/generated-sources/jaxb/icaro/aplicaciones/descripcionorganizaciones/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.5-2 
-// Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: PM.10.07 a las 01:34:06 PM CEST 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "urn:icaro:aplicaciones:descripcionOrganizaciones", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package icaro.aplicaciones.descripcionorganizaciones;
