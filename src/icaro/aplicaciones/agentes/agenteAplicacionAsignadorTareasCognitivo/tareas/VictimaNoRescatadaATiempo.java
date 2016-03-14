fec2f2b7dcd2060133e0507edbaec98a9c28af17
Luis García <luigiter@hotmail.com>
2016-03-14 09:35:22 +0100

    Reparacion bug en nueva tarea VictimaNoRescatadaATiempo

--- src/icaro/aplicaciones/agentes/agenteAplicacionAsignadorTareasCognitivo/tareas/VictimaNoRescatadaATiempo.java
+++ src/icaro/aplicaciones/agentes/agenteAplicacionAsignadorTareasCognitivo/tareas/VictimaNoRescatadaATiempo.java
@@ -14,22 +14,22 @@ public class VictimaNoRescatadaATiempo extends TareaSincrona{
 		// TODO Auto-generated method stub
 		Informe informe = (Informe) params[0];
 		Victim victima = (Victim) params[1];
-		
-		
+
+
 		String nombreVictima = informe.identEntidadEmisora;
 		Object[] valoresParametrosAccion = new Object[1];
 		valoresParametrosAccion[0]=nombreVictima;
 		String robotIdEncargado = victima.getIdRobotEncargadoDeMi();
-		
-		this.comunicator.enviarInfoAotroAgente(new Informe(this.identAgente, nombreVictima,VocabularioRosace.MsgVictimaMuertaASubordinado),robotIdEncargado);
-		
+		if(robotIdEncargado != null)
+			this.comunicator.enviarInfoAotroAgente(new Informe(this.identAgente, nombreVictima,VocabularioRosace.MsgVictimaMuertaASubordinado),robotIdEncargado);
+
 		InfoContEvtMsgAgteReactivo msg = new InfoContEvtMsgAgteReactivo("victimaMuerta", valoresParametrosAccion);
 		this.comunicator.enviarInfoAotroAgente(msg, VocabularioRosace.IdentAgteControladorSimulador);
-		
-		
-		
-		
-		
+
+
+
+
+
 	}
 
 }
