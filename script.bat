b9e70fb2dbaeb127ec3309c6d16b22ed4dcc846e
Luis <luigiter@hotmail.com>
2016-05-27 11:08:51 +0200

    Nuevos escenarios

--- script.bat
+++ script.bat
@@ -1,4 +1,6 @@
 FOR %%A IN (1,2,3) DO java -jar demo.jar descripcion_4robots_10victimas_CASA %%A
+FOR %%A IN (1,2,3) DO java -jar demo.jar descripcion_4robots_15victimas_CASA %%A
+FOR %%A IN (1,2,3) DO java -jar demo.jar descripcion_4robots_20victimas_CASA %%A
 FOR %%A IN (1,2,3) DO java -jar demo.jar descripcion_6robots_10victimas_CASA %%A
 FOR %%A IN (1,2,3) DO java -jar demo.jar descripcion_8robots_10victimas_CASA %%A
 FOR %%A IN (1,2,3) DO java -jar demo.jar descripcion_6robots_16victimas_CASA %%A
@@ -9,4 +11,5 @@ FOR %%A IN (1,2,3) DO java -jar demo.jar descripcion_6robots_12victimas_LLANURA 
 FOR %%A IN (1,2,3) DO java -jar demo.jar descripcion_4robots_14victimas_ESTADIO %%A
 FOR %%A IN (1,2,3) DO java -jar demo.jar descripcion_6robots_14victimas_ESTADIO %%A
 FOR %%A IN (1,2,3) DO java -jar demo.jar descripcion_8robots_14victimas_ESTADIO %%A
+FOR %%A IN (1,2,3) DO java -jar demo.jar descripcion_8robots_17victimas_ESTADIO %%A
 FOR %%A IN (1,2,3) DO java -jar demo.jar descripcion_8robots_20victimas_ESTADIO %%A
